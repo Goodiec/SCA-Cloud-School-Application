9149cbc8c01b3a0e1f9d0f432bb6f23c7de27f61
Goodness chris-ugari <goodnesschrisugari@yahoo.com>
2021-01-14 14:42:17 +0100

    modified text

--- docker/app.js
+++ docker/app.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Welcome to SCA Cloud School Application');
+  res.send('Welcome to SCA Cloud School Application, this is my first assessment');
 });
 
 app.listen(PORT, HOST);
